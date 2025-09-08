0c5d87490435f240238f9b089e613cbf3644409c
MaxxShadow <99481513+MaxxShadow@users.noreply.github.com>
2025-09-08 08:26:00 +0200

    Create konverze-folder-multisheet.py

---
+++ Konverze-FO-MIG/konverze-folder-multisheet.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import string
+import sys
+import os
+import argparse
+
+def get_files_in_directory(directory, file_type):
+    files = [f for f in os.listdir(directory) if f.endswith(file_type)]
+    file_names = [os.path.splitext(f)[0] for f in files]
+    data = {'INEXCELNAME': files, 'OUTCSVNAME': file_names}
+    df = pd.DataFrame(data)
+    return df
+
+directoryXLS = '.\\XLS'
+file_type = '.xlsx'
+repString1 = 'File:\n {}\n.sheet:\n {}\n---konverted---\nto file:\n {}'
+
+ListExcel = get_files_in_directory(directoryXLS, file_type)
+
+defined_sheets = ['ECZ', 'ESK', 'EHO']
+for l in ListExcel.index:
+    InFileXls = '.\\XLS\\' + ListExcel.at[l, 'INEXCELNAME']
+    xls = pd.ExcelFile(InFileXls)
+    sheets = xls.sheet_names
+    for SheetName in sheets:
+      if SheetName in defined_sheets:
+        OutFileCsv = '.\\CSV\\' + SheetName + '-' + ListExcel.at[l, 'OUTCSVNAME']+'.csv'
+        tabulka = pd.read_excel(InFileXls
+                    , sheet_name=SheetName
+                    , na_values='#N/A'
+                    , index_col=None
+                    , dtype = str
+                    #, converters={'MAINACCOUNTID':str}
+                    #, usecols="A,H,K:M,S"
+                    #, usecols="A"
+                    #, usecols=None
+                    )
+        tabulka.to_csv(OutFileCsv, sep='|', encoding='utf-16le', na_rep='', index=False)
+        print(repString1.format(ListExcel.at[l, 'INEXCELNAME'], SheetName, OutFileCsv))
+        print('---')
